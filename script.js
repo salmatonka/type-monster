90be8f4b3ecc955d33acb771cbfaf313cabe8ac7
salmatonka <108433658+salmatonka@users.noreply.github.com>
2022-09-14 22:44:04 +0600

    second commit

--- script.js
+++ script.js
@@ -105,9 +105,9 @@ const start = () => {
 
   let count = 3;
   countdownOverlay.style.display = "flex";
-
+  
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
@@ -124,7 +124,7 @@ const start = () => {
 };
 
 // START Countdown
-startBtn.addEventListener("click", start);
+ startBtn.addEventListener("click", start);
 
 // If history exists, show it
 displayHistory();
